a5bced47b7fd00012c645a16e3ede555cb07da57
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-11-27 23:38:05 -0600

    Import Assets and Implement Basic Movement

---
+++ Final Project Bucio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpForce;
+
+    private Rigidbody2D playerRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+        }
+    }
+}
