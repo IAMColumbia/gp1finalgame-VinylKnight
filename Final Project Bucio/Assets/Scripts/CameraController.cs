edc49f1392b2dc4bfc509d842aa06a09a56a2fdc
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-11-30 03:34:31 -0600

    Jump Animation and Refine Jump and Movement

---
+++ Final Project Bucio/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+
+    private Vector3 lastPlayerPositition;
+    private float distanceToMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        lastPlayerPositition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToMove = player.transform.position.x - lastPlayerPositition.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPositition = player.transform.position;
+    }
+}
--- Final Project Bucio/Assets/Scripts/PlayerController.cs
+++ Final Project Bucio/Assets/Scripts/PlayerController.cs
@@ -8,22 +8,36 @@ public class PlayerController : MonoBehaviour
     float speed;
     [SerializeField]
     float jumpForce;
+    [SerializeField]
+    private bool isOnGround;
+    [SerializeField]
+    private LayerMask defineGround;
+
 
     private Rigidbody2D playerRigidBody;
+    private Collider2D playerCollider;
+    private Animator playerAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidBody = this.GetComponent<Rigidbody2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isOnGround = Physics2D.IsTouchingLayers(playerCollider, defineGround);
+
         playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
         if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
         {
+            if(isOnGround)
             playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
         }
+        playerAnimator.SetFloat("speed", playerRigidBody.velocity.x);
+        playerAnimator.SetBool("Ground", isOnGround);
     }
 }
